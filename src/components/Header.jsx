5f79fd4856dbceecab75b3fb7ed01e5045f21444
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-16 19:12:11 +0300

    added z index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import headerImg from "../assets/header.jpg";
 import "../styles/header.css";
 
 const Header = () => {
   return (
     <div>
-      <div className="headerContainer"></div>
+      <div className="headerContainer">
+        <div className="headerImgContainer">
+          <div className="headerTextContainer">
+            <h1>Construction</h1>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+              assumenda, quibusdam sequi nisi nostrum autem cumque ipsa cum
+              laudantium aut voluptate ducimus animi dolor nihil magni
+              reprehenderit maxime eum adipisci?
+            </h3>
+          </div>
+          <img id="headerImage" src={headerImg} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
