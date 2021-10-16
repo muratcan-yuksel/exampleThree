7840936931f9394d0e58c3e764ff49bf356f43de
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-16 22:10:30 +0300

    wonderfully responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import headerImg from "../assets/header.jpg";
 import "../styles/header.css";
-// import icon1 from "../assets/icon1.svg";
-// import icon2 from "../assets/icon2.svg";
-// import icon3 from "../assets/icon3.svg";
-// import icon4 from "../assets/icon4.svg";
+import icon1 from "../assets/icon1.svg";
+import icon2 from "../assets/icon2.svg";
+import icon3 from "../assets/icon3.svg";
+import icon4 from "../assets/icon4.svg";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
@@ -24,7 +24,49 @@ const Header = () => {
             </div>
             <img id="headerImage" src={headerImg} alt="" />
           </div>
-          <div className="servicesSection col-12 col-sm-3"></div>
+          <div className="servicesSection col-12 col-sm-3">
+            <h1 className="servicesTitle">Our Services</h1>
+            <div className="serviceBundle">
+              <div className="serviceIcon">
+                {" "}
+                <img src={icon1} alt="" />
+              </div>
+              <div className="bundleTexts">
+                <div className="bundleTitle">Title</div>
+                <div className="bundleText">Lorem ipsum dolor sit amet</div>
+              </div>
+            </div>
+            <div className="serviceBundle">
+              <div className="serviceIcon">
+                {" "}
+                <img src={icon2} alt="" />
+              </div>
+              <div className="bundleTexts">
+                <div className="bundleTitle">Title</div>
+                <div className="bundleText">Lorem ipsum dolor sit amet</div>
+              </div>
+            </div>
+            <div className="serviceBundle">
+              <div className="serviceIcon">
+                {" "}
+                <img src={icon3} alt="" />
+              </div>
+              <div className="bundleTexts">
+                <div className="bundleTitle">Title</div>
+                <div className="bundleText">Lorem ipsum dolor sit amet</div>
+              </div>
+            </div>
+            <div className="serviceBundle">
+              <div className="serviceIcon">
+                {" "}
+                <img src={icon4} alt="" />
+              </div>
+              <div className="bundleTexts">
+                <div className="bundleTitle">Title</div>
+                <div className="bundleText">Lorem ipsum dolor sit amet</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
