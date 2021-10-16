64fdc0c8b0e0d5bf514a18c19d1e6a90b7e7091d
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-16 18:17:08 +0300

    restarting the project + added vivid icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,10 @@
 import React from "react";
-import headerImg from "../assets/header.jpg";
 import "../styles/header.css";
 
 const Header = () => {
   return (
     <div>
-      <div className="headerContainer">
-        <img id="headerImage" src={headerImg} alt="" />
-      </div>
+      <div className="headerContainer"></div>
     </div>
   );
 };
