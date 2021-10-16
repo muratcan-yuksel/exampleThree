ae8c0a8ec3582cd1344096b96f9920f04deeb664
muratcan-yuksel <muratcanyukselpro@gmail.com>
2021-10-16 23:53:06 +0300

    added navbar but of course dropdown doesnt work

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Header />
       <Projects />
     </div>
